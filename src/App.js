bd5afb1fa0329e9d2ee2f65b64f5e41c51cfcfc8
esionascimento <esio.392@protonmail.com>
2021-10-09 03:53:28 -0400

    add filer Context/auth.js / dois context

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import StoreProvider from './components/Context/Provider';
+import { AuthProvider } from './components/Context/auth'
 import RoutesPrivate from './components/Routes/Private/Private';
 import Home from './pages/Home/Home.js'
 import Cadastro from './components/Cadastro/Cadastro'
@@ -11,14 +12,16 @@ import NotFound from './pages/NotFound.js';
 const App = () => {
   return (
     <BrowserRouter>
-      <StoreProvider>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <RoutesPrivate exact path="/dashboard" component={() => <Dashboard />} />
-          <Route exact path="/cadastro" component={() => <Cadastro />} />
-          <Route exact path="/not-found" component={() => <NotFound />} />
-        </Switch>
-      </StoreProvider>
+      <AuthProvider>
+        <StoreProvider>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <RoutesPrivate exact path="/dashboard" component={() => <Dashboard />} />
+            <Route exact path="/cadastro" component={() => <Cadastro />} />
+            <Route exact path="/not-found" component={() => <NotFound />} />
+          </Switch>
+        </StoreProvider>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
