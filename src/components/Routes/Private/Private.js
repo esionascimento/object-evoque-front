7e5c28a4f516732c5078a9c822fb6acc7ec1608e
esionascimento <esio.392@protonmail.com>
2021-10-13 07:52:26 -0400

    isAuthenticated funcionando, corrigir depois o validade do jwt

--- src/components/Routes/Private/Private.js
+++ src/components/Routes/Private/Private.js
@@ -1,11 +1,11 @@
-import React/* , { useContext } */ from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
-/* import { AuthContext } from '../../Context/auth'; */
 import { useSelector } from 'react-redux';
 
 const RoutesPrivate = ({ component: Component, ...rest }) => {
   const { isAuthenticated } = useSelector(state => state.auth);
+  console.log('isAuthenticated :', isAuthenticated);
 
   return (
     <Route
