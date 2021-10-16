ac65ecd3a9eda7debf806b559ecebbf585df927c
esionascimento <esio.392@protonmail.com>
2021-10-16 17:16:08 -0400

    add divs body

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -5,6 +5,7 @@ import './Dashboard.css';
 function Dashboard() {
   
   return (
+    <>
       <header className="app-header">
         <ul className="div">
           <li>Home</li>
@@ -17,6 +18,17 @@ function Dashboard() {
           <li>Sair da Conta</li>
         </ul>
       </header>
+      <div className="body">
+        <div className="esquerda">
+          <div>Esquerda</div>
+          <div></div>
+        </div>
+        <div className="direita">
+          <div>Direita</div>
+          <div></div>
+        </div>
+      </div>
+    </>
   );
 }
 
