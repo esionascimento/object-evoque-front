dd3827b1c23e127a18d8051659928b7c2d949416
esionascimento <esio.392@protonmail.com>
2021-10-19 17:37:33 -0400

    rota / redirecionar para /dasboard caso jwt estiver valido, em andamento

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import store from './store/store';
 import { Provider } from 'react-redux';
 
-/* import StoreProvider from './components/Context/Provider';
-import { AuthProvider } from './components/Context/auth' */
 import RoutesPrivate from './components/Routes/Private/Private';
+import { LoginAuth } from './components/Routes/LoginAuth';
 
 import Home from './pages/Home/Home.js'
 import Cadastro from './components/Cadastro/Cadastro'
@@ -18,7 +17,7 @@ const App = () => {
     <BrowserRouter>
       <Provider store={store}>
           <Switch>
-            <Route exact path="/" component={Home} />
+            <LoginAuth exact path="/" component={Home} />
             <RoutesPrivate exact path="/dashboard" component={() => <Dashboard />} />
             <Route exact path="/cadastro" component={() => <Cadastro />} />
             <Route exact path="/not-found" component={() => <NotFound />} />
--- src/components/Routes/LoginAuth.js
+++ src/components/Routes/LoginAuth.js
@@ -3,23 +3,20 @@ import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-const RoutesPrivate = ({ component: Component, ...rest }) => {
+export const LoginAuth = ({ component: Component, ...rest }) => {
   const { isAuthenticated } = useSelector(state => state.auth);
-  
   return (
     <Route
       {...rest}
       render={(props) => isAuthenticated
-        ? <Component {...rest} />
-        : <Redirect to={{ pathname: "/not-found", state: { from: props.location } } }/>
+        ? <Redirect to={{ pathname: "/dasboard", state: { from: props.location } } }/>
+        : <Component {...rest} />
       }
     />
   )
 }
 
-RoutesPrivate.propTypes = {
+LoginAuth.propTypes = {
   component: PropTypes.func.isRequired,
   location: PropTypes.func,
 }
-
-export default RoutesPrivate;
